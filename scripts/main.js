7f8a1ab975e48f11169afc7c06a3628061531c58
Your Name <getbackwards@gmail.com>
2021-02-03 21:30:32 +1300

    adding a bit of stuff

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,42 @@
+const contentHeading = document.getElementById('content-heading');
 const headerEl = document.getElementById('header-el');
 const panelButton = document.getElementById('panel-button');
 const searchInput = document.getElementById('search-input');
 
-const addLinearBg = () => {
-    headerEl.classList.add('linear-background');
-}
+const addBgFade = () => {
+  headerEl.classList.add('background--transition');
+};
 
-const removeLinearBg = () => {
-    headerEl.classList.remove('linear-background');
-}
+const nav =
+  '<div class="content">' +
+  '<nav class="content__nav">' +
+  '<ul>' +
+  '<li>List item</li>' +
+  '<li>List item</li>' +
+  '<li>List item</li>' +
+  '<ul>' +
+  '</nav>';
 
-searchInput.addEventListener('focus', addLinearBg);
-searchInput.addEventListener('blur', removeLinearBg);
+// const addNav = () => {
+//     contentHeading.insertAdjacentHTML('afterend', nav);
+// }
+
+const addNav = () => {
+  contentHeading.insertAdjacentHTML('afterend', nav);
+};
+
+const removeBgFade = () => {
+  headerEl.classList.remove('background--transition');
+};
+
+// panelButton.addEventListener('click', addNav);
+searchInput.addEventListener('focus', addBgFade);
+searchInput.addEventListener('blur', removeBgFade);
+
+panelButton.addEventListener(
+  'click',
+  function (e) {
+    addNav();
+  },
+  { once: true }
+);
